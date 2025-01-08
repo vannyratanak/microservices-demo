d659422aa772af148d4048fd3e08b22555b5810d
vannyratanak <vannyratanakchheang@gmail.com>
2025-01-08 06:01:12 +0000

    deployment gke

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+# kubectl apply -f ./monitoring/monitoring.yaml
+kubectl apply -f ./release/kubernetes-manifests.yaml
+# kubectl apply -f ./monitoring/prometheus/
+# kubectl apply -f ./monitoring/grafana/grafana.yaml
