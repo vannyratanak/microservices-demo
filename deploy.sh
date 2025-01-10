42826a420fb0c1cca930cc6a76b246a22ffb028c
vannyratanak <vannyratanakchheang@gmail.com>
2025-01-10 12:43:22 +0000

    add script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
-# kubectl apply -f ./monitoring/monitoring.yaml
 kubectl apply -f ./release/kubernetes-manifests.yaml
-# kubectl apply -f ./monitoring/prometheus/
-# kubectl apply -f ./monitoring/grafana/grafana.yaml
+
